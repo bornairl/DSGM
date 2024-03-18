9eb2e99f251823c00bacf1450b32b854daf10056
SergioMaceda <macsergg@gmail.com>
2024-03-18 10:58:03 -0400

    calculator

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+#Sergio Maceda
+#Calculator to add two number and display the result
+
+#1. Input
+x1 = Input ("Type x1: ")
+x2 = Input ("Type x2: ")
+
+#Process
+
+sum = int(x1) + int(x2) 
+
+#output
+
+Print (f"sum: {sum}")
